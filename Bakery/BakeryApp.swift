0c1e9367c58ec4cb3da887f7c19fbc1eb48117be
maram <ma.ram.5.sky5@gmail.com>
2025-12-23 10:29:31 +0300

    Initial Commit

---
+++ Bakery/BakeryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BakeryApp.swift
+//  Bakery
+//
+//  Created by Maram Ibrahim  on 03/07/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct BakeryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bakery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bakery
+//
+//  Created by Maram Ibrahim  on 03/07/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
